5734103458aacb9b1cf961a73b041caf220b43b8
MissVintee <vintee.c23@gmail.com>
2024-08-26 19:31:33 +0530

    Hello to Node.js

---
+++ helloJS/hello.js
@@ -0,0 +1,12 @@
+//importing
+const math = require ('./math');
+// console.log('Hello , world!'); // outputs: Hello, world!
+
+// console.log("value of add operation is: ", math.add(35,4));
+// console.log(  "value of subtract operation is: ", math.sub(30,4));
+
+// //destructuring method
+// const {add, sub, mul,  div} = math;
+// console.log("value of math is: ", mul(2,4));
+
+console.log( math.power(2,2))
